880e6ae90b25468ba70853db1a0674a3c4e5140a
mmoulin <mmoulin@openstudio.fr>
2022-04-26 15:34:24 +0200

    gestion States concurrents

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './Button.module.css';
 import PropTypes from 'prop-types'
 
 const Button =(props)=>{
     const [isClicked, setClicked] = useState(false);
+    const [isHovered, setHovered] = useState(false);
+    useEffect(() => {
+        console.log('update state', isClicked);
+        if (isClicked) {
+            setTimeout(() => {
+                setClicked(false);
+            }, 1000);
+        }
+    }, [isClicked, isHovered]);
     return (
         <button
-            className={style.Button}
+            className={`${style.Button}${isClicked ? ' ' + style.clicked : isHovered ? ' ' + style.hovered : ''}`}
             style={ {color:props.color, backgroundColor:props.bgColor} }
             onClick={(evt)=>{
                 //console.log(evt);
-                setClicked(!isClicked);
-                console.log("isClicked", isClicked);
+                setClicked(true);
+                //console.log("isClicked", isClicked);
+            }}
+            onMouseOver={(evt)=>{
+                //console.log(evt);
+                setHovered(true);
+                //console.log("isHovered", isHovered);
+            }}
+            onMouseLeave={(evt)=>{
+                //console.log(evt);
+                setHovered(false);
+                //console.log("isHovered", isHovered);
             }}
         >
             {props.children}
